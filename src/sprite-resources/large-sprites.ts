7710f7aa284fcd0c643e5b6b93d79db125f5323f
Marianne Hval <mariannehval@gmail.com>
2022-10-24 22:19:17 +0200

    Added Jolteon

--- src/sprite-resources/large-sprites.ts
+++ src/sprite-resources/large-sprites.ts
@@ -5,4 +5,8 @@ export const LARGE_SPRITES: Sprite[] = [
     name: 'Flareon',
     imageUrl: 'https://mahval-large-sprites.s3.eu-west-1.amazonaws.com/136Flareon.png',
   },
+  {
+    name: 'Jolteon',
+    imageUrl: 'https://mahval-large-sprites.s3.eu-west-1.amazonaws.com/135Jolteon.png',
+  },
 ];
